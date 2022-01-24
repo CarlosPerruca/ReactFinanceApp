3b2a0c40c12017e02b4b518da255da7c6cea1621
Carlos P <carlos_dic1985@hotmail.com>
2022-01-24 16:27:33 +0100

    adding styles to the expenseItems component

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,5 +1,15 @@
+import "./ExpenseItems.css";
+
 function ExpenseItems() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>January 24th 2022</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item__price">199.65€</div>
+    </div>
+  );
 }
 
 export default ExpenseItems;
