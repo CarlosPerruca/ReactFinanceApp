e1ed463e1c458cc98f4f3161ea23ee54ff8d5c39
Carlos P <carlos_dic1985@hotmail.com>
2022-01-27 12:02:36 +0100

    date format

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,10 +1,18 @@
 import "./ExpenseItems.css";
 
 function ExpenseItems(props) {
+  let day = props.date.toLocaleString("en-UE", { day: "2-digit" });
+  let month = props.date.toLocaleString("en-UE", { month: "long" });
+  let year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
+
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
