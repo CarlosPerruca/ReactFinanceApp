d07417d6098aada09e8f86fb312b724c6f9b881e
Carlos P <carlos_dic1985@hotmail.com>
2022-01-27 11:16:25 +0100

    components with props

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,13 +1,14 @@
 import "./ExpenseItems.css";
 
-function ExpenseItems() {
+function ExpenseItems(props) {
+
   return (
     <div className="expense-item">
-      <div>January 24th 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">199.65€</div>
+      <div className="expense-item__price">{props.amount}€</div>
     </div>
   );
 }
