20af79ce272c70d7b98eebd3f654735de3ff57ef
Carlos P <carlos_dic1985@hotmail.com>
2022-02-14 17:32:16 +0100

    working on add datas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import React from "react";
 
 const App = () => {
-  let expenses = [
+  const expenses = [
     {
       id: "e1",
       title: "Toilet Paper",
@@ -23,9 +24,14 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In app.js");
+    console.log(expense);
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
